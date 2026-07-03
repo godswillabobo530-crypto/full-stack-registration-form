031ba0662d6a14c43720b748c8b08960bcf6f7cb
Godswill <godswillabobo530@gmail.com>
2026-07-03 13:42:52 +0100

    Update server.js

--- backend/backend/backend/backend/server.js
+++ backend/backend/backend/backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
